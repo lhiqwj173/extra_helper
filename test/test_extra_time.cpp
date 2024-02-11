17b3acd30c3f0a1853ccde8ee6c145c7e08776ee
lhiqwj173 <lhssqtz@gmail.com>
2024-02-11 09:03:36 +0800

    动态库

--- test/test_extra_time.cpp
+++ test/test_extra_time.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 int main() {
+	cout << "²âÊÔ strptime" << endl;
 	extra::time::timestamp_ms t = 1707571939155;
 	auto t_str = extra::time::strptime(t);
 	cout << t << " -> \n\t"
